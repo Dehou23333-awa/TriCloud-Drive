e44c80e7ea62c4795a06a456ba393bccdbddeae9
大聪明 <biggersmart@outlook.com>
2025-09-26 20:08:16 +0800

    build: 更新 .gitignore 并修改 nuxt 配置
    - 更新 .gitignore 以忽略 .output.7z 文件
    - 修改 nuxt.config.ts 以使用 node-server 预设

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
   modules: ['@nuxtjs/tailwindcss'],
 
   nitro: {
-    preset: 'node',
+    preset: 'node-server',
     experimental: {
       wasm: true
     },
